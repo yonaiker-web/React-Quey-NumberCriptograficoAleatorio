956f2472b799ad3c0e0e7a8da09ced3705962cd0
yonaiker <yonaiker.ocando@unidigital.global>
2023-06-09 12:20:21 -0400

    .

--- src/AppOld.tsx
+++ src/AppOld.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useReducer, useState } from "react";
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
 
-//creo la funcion que llamara el numero aleatorio usando un api
+//creo la funcio que llamara el numero aleatorio usando un api
 //():Promise<number> va a retorna una promesa y esa promesa va a resolver un numero
 const getNumberRandom = async (): Promise<number> => {
   //hacemos la peticion
@@ -19,28 +18,53 @@ const getNumberRandom = async (): Promise<number> => {
   return +numberString;
 };
 
-export const App = () => {
-  //el primer arreglo [] es como queremos que react-query manejara el cache
-  //segundo es la funcion que se usara para cargar la informacion
-  //query optendara todo lo relacionado con la petecion.
-  const query = useQuery(["ramdomNumber"], getNumberRandom);
+export const AppOld = () => {
+  //estado que almacena el numoer aleatorio tipandolo que sea un dato solo numerico
+  const [number, setNumber] = useState<number>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string>();
+  //hace el llamado de la peticion fetch
+  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
+
+  //cuando se monte el componente ejecuta esto de primero que resuleve la promesa del numero aleatorio via api
+  useEffect(() => {
+    setIsLoading(true);
+    //llamamos a la funcion que trae el numero aleatorio y cuando finalice de resolver lo que hace lo guardamos en el state
+    getNumberRandom()
+      //si se resuelve bien
+      .then(setNumber)
+      //si da error capturame le error y lo guardamos el mjs del error en el estado de error
+      .catch((error) => setError(error.message));
+
+    //agregamos el key de un reducer para forzar el cambio de esta accion
+  }, [key]);
+
+  //accion que se realiza si el estado del numero cambia
+  useEffect(() => {
+    if (number) {
+      setIsLoading(false);
+    }
+  }, [number]);
+
+  //accion si hay un error
+  useEffect(() => {
+    if (error) {
+      setIsLoading(false);
+    }
+  }, [error]);
 
   return (
     <div className="App App-header">
       {/* si isLoading es verdadera muestra el cargando */}
-      {query.isFetching ? (
-        <h2>Cargando...</h2>
-      ) : (
-        <h2>Numero Aleatorio: {query.data}</h2>
-      )}
+      {isLoading ? <h2>Cargando...</h2> : <h2>Numero Aleatorio: {number}</h2>}
 
-      {!query.isLoading && query.isError && <h3>{`${query.error}`}</h3>}
+      {!isLoading && error && <h3>{error}</h3>}
 
-      <button onClick={() => query.refetch()} disabled={query.isFetching}>
-        {query.isFetching ? "..." : "Nuevo Numero"}
+      <button onClick={forceRefetch} disabled={isLoading}>
+        {isLoading ? "..." : "Nuevo Numero"}
       </button>
     </div>
   );
 };
 
-export default App;
+export default AppOld;
